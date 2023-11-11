c9e71897689e59a2d3d0788da0cb9e9fed92f040
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-12 01:51:24 +0600

    how to Print in C++

---
+++ ouput.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+
+int main(){
+
+
+
+    int a=10;
+
+    long long int b = 33472834283483;
+
+    float d= 1.53;
+
+    double ccc= 'A';
+
+
+    //the c way was printf
+
+    // waht if I want to use quotation as well??
+
+    std:: cout<< "my  favo number is "<<a<<"\n";
+
+    std:: cout<< "my number should be appear here "<<a<< " faovite is ";
+    // std:: cout<<a;
+    
+    std::cout<<a<<" "<<b;
+
+
+    std:: cout<<a<<"\n"<<b;
+
+
+    return 0;
+
+
+}
