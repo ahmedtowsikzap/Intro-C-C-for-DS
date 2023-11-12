02a75e29334ad04ab72ffe4db129baf994252105
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-13 00:48:12 +0600

    if else normal condition

---
+++ condition_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+ int x=10;
+
+ if(x==10){
+
+    cout<<"phew!!!"<<endl;
+ }
+ else{
+    cout<<"this is not ten"<<endl;
+ }
+
+    return 0;
+}
