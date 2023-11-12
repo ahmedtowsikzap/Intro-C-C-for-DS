e55518b28030381f1037bab9f1158fd506c82578
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-12 22:21:55 +0600

    EOF in C++

---
+++ eof.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int a, b;
+ 
+  while(cin>>a>>b){ // for EOF we don't need to say the end of file stuffs 
+  // c++ automatically detect it right away!!
+
+
+  }
+    
+    return 0;
+}
+
