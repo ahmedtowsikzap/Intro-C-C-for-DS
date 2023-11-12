c8326bafee90325e40f8669db8a7ff914547faf4
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-12 21:53:16 +0600

    takeinput and conversion using namespace and many more

---
+++ takeinput.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+ long long int b;
+
+//  cin>>a>>b;
+
+
+
+//  cout<<a<< " "<< b<<endl;
+
+     int a;
+     char c;
+
+
+     cin>>a>>c;
+
+    //  int ascii = int(c);
+
+     cout<<a<<" "<<c;
+}
