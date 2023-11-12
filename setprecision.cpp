20b72f0868760bd559d5f6d5e7bd353aca9f4723
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-13 00:45:04 +0600

    setprecision & how it works

--- setprecision.cpp
+++ setprecision.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main(){
 
+   double a;
 
+   cin>>a;
+
+   cout<<fixed<<setprecision(5)<<a<<endl;
+
+    
+
+ 
+   return 0;
     
 }
