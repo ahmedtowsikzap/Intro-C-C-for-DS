81d11da56ac858c7246dc8c5be3625a9bf0b367f
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-13 02:00:13 +0600

    swap function and its usage

---
+++ swap.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<utility>
+using namespace std;
+
+
+// void my_swap(int *a, int *b){
+// int tmp=*a;
+// *a=*b;
+// *b=tmp;
+
+// }
+
+int main(){
+
+int a,b;
+
+cin>>a>>b;
+ swap(b,a);
+// int tmp=a;
+// a=b;
+// b=tmp;
+
+
+cout<<a<<" "<<b;
+
+return 0;
+}
