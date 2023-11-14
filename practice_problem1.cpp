9fbafade7a578bf035d1919d3b551b7d625bc6bf
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-15 00:27:37 +0600

    practice problems 1

---
+++ practice_problem1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int* get_array(int n){
+
+    int *p = new int[n];
+    for(int i=0; i<n; i++){
+
+        cin>>p[i];
+    }
+    return p;
+
+}
+
+
+int main(){
+
+int n;
+
+cin>>n;
+
+int *d = get_array(n);
+
+for(int i=0; i<n;i++){
+
+    cout<<d[i]<<' ';
+}
+
+    return 0;
+
+
+}
