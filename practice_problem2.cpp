c0fe34418eae8e034367bcd5a1b049e17b07db44
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-15 01:40:01 +0600

    practice problems stuck on the copying problems

---
+++ practice_problem2.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int n;
+int m;
+
+cin>>n;
+
+
+int *A = new int[n];
+int *B = new int[m];
+
+
+for(int i=0; i<n; i++){
+
+    cin>>A[i];
+    B[i] = A[i];
+
+}
+
+
+cin>>m;
+
+B = new int[m];
+
+delete[]A;
+
+
+for(int i=0; i<m; i++){
+
+
+
+}
+
+
+
+    return 0;
+}
