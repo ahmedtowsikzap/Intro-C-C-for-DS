68d993a0065851073f9b16e078b6548f01dc5820
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-14 22:02:03 +0600

    min_ and max findings using c++ and built in function using max() and also using min function as well

---
+++ E_Max.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<limits>
+
+using namespace std;
+
+
+int main() {
+
+   int n;
+
+   cin>>n;
+
+   int arr[n];
+
+
+   for(int i=0; i<n; i++){
+
+
+    cin>>arr[i];
+   }
+
+int maxx = INT_MIN;
+
+
+for(int i=0; i<n; i++){
+
+    maxx= max(maxx, arr[i]);
+}
+
+
+cout<<maxx<<endl;
+
+
+ 
+
+    return 0;
+}
