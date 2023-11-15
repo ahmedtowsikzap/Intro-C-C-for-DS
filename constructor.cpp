53abd8ba2401a8db5762911c93386997c652ab3e
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-16 01:51:51 +0600

    learning constructor

---
+++ constructor.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+
+    public:
+
+
+    int roll;
+    int cls;
+    double gpa;
+
+
+};
+
+int main(){
+    
+ Student ahmed;
+
+ ahmed.roll = 29;
+ahmed.cls=9;
+ahmed.gpa = 5.77;
+
+
+Student rahim;
+
+
+rahim.roll = 45;
+
+rahim.cls = 10;
+
+rahim.gpa = 4.55;
+    return 0;
+
+}
