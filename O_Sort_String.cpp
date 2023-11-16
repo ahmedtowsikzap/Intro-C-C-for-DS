2d0214c8f1f41c41d3fce6636bae1edf891f148c
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-16 22:30:32 +0600

    O_sort string using frequency array

--- O_Sort_String.cpp
+++ O_Sort_String.cpp
@@ -4,6 +4,32 @@ using namespace std;
 int main(){
     
 
+    int n;
+
+    cin>>n;
+
+ int freq[26] = {0};
+
+ for(int i=0; i<n; i++){
+
+    char S;
+    cin>>S;
+
+int value = S-'a';
+freq[value]++;
+
+ }
+
+ for(int i=0; i<26; i++){
+
+    for(int j=0; j<freq[i]; j++){
+
+        char p;
+        p = i+'a';
+        cout<<p;
+    }
+ }
+
     return 0;
 
 }
