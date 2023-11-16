ceefd4f151bb14251e0d4ccea35dc9bd23dfe7bd
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-16 17:32:53 +0600

    objects values receiving by using dynamic objects and usage of dynamic objects and later deleting the dynamic or clearning the dynamic memeory

--- reutrnfunc.cpp
+++ reutrnfunc.cpp
@@ -19,15 +19,17 @@ class Student{
 
 Student* fun(){
 
-Student ahmed(21, 1, 3.67); // my memory is deleting the values or not retruning any values because of jumping fron function one to another 
+// Student ahmed(21, 1, 3.67); // my memory is deleting the values or not retruning any values because of jumping fron function one to another 
 
-Student *p = &ahmed;
-return p;
+Student *ahmed = new Student(343, 1, 3.98);
+
+return ahmed;
 
 }
 int main(){
     
   Student* result=fun(); // when we are receiving the values here its basically empty!! and there is nothing there anymore even though I f I have the address of the value of ahmed object but in the end it dosent matter cause I lost the values that I gave before!!
+delete result;
 
   cout<<(*result).age<<" "<< (*result).semester<<" "<<(*result).gpa;
 
