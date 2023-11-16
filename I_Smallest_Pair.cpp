26b340253349fb2dbdb0e598deaedb64e551961b
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-16 21:49:07 +0600

    I smallest pair

--- I_Smallest_Pair.cpp
+++ I_Smallest_Pair.cpp
@@ -1,35 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
-   int t;
-   cin>>t;
-   while(t--){
-     int n;
+int main()
+{
 
-    cin>>n;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
 
-    int a[n];
+        cin >> n;
 
-    for(int i=0; i<n; i++){
-        cin>>a[i];
-    }
-    int min_value = INT_MAX;
+        int a[n];
 
-    for(int i=0; i<n-1; i++){
-
-        for(int j=i+1; j<n; j++){
-
-           int value = a[i]+a[j]+(j+1)-(i+1);
-            min_value = min(min_value,value);
-
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
         }
+        int min_value = INT_MAX;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+
+            for (int j = i + 1; j < n; j++)
+            {
+
+                int value = a[i] + a[j] + (j + 1) - (i + 1);
+                min_value = min(min_value, value);
+            }
+        }
+
+        cout << min_value<<endl;
     }
-    
-    cout<<min_value;
-   }
 
     return 0;
-
 }
