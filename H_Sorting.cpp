1c2bbfe047ce51858655ba19d050823910dfdc18
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-16 22:49:24 +0600

    H_sorting using built in sort function

--- H_Sorting.cpp
+++ H_Sorting.cpp
@@ -19,13 +19,11 @@ int main(){
 
 for(int i=0;i<n-1; i++){
     for(int j=i+1; j<n; j++){
-        if(arr[i]>arr[j]){
-            swap(arr[i],arr[j]);
+            sort(arr,arr+n);
         }
     }
-    for(int i=0; i<n; i++){
-
-        cout<<arr[i]<<endl;
+    for(int i=0; i<n-1; i++){
+        cout<<arr[i]<<" ";
     }
-}
+    cout << arr[n - 1];
 }
