7a0ebf076e0b6796276c4d8f7ebeb8aaba0f7205
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-21 02:41:38 +0600

    string_constructor and many more

---
+++ string_constructor.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // string s("hello world");
+    // cout<<s<<endl;
+
+    string s("hellow owrd", 4);
+    cout<<s<<endl;
+    
+
+
+
+    return 0;
+
+}
