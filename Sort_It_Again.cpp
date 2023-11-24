80db2b9a562818cf4e0ab660d85ef987442608d0
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-25 01:34:22 +0600

    sort it again

---
+++ Sort_It_Again.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+class students{
+
+    public:
+    string name;
+    int cls;
+    string section;
+    int id;
+    int math_marks;
+    int eng_marks;
+};
+
+
+int main(){
+    
+     int n;
+     cin>>n;
+
+     students student[n];
+     for(int i=0; i<n; i++){
+
+        cin>>student[i].name>>student[i].cls>>student[i].section>>student[i].id>>student[i].math_marks>>student[i].eng_marks;
+     }
+
+     for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+
+        if(student[i].eng_marks < student[j].math_marks){
+                swap(student[i], student[j]);
+        }
+        if(student[i].eng_marks == student[j].eng_marks){
+            if(student[i].math_marks < student[j].math_marks){
+                swap(student[i], student[j]);
+            }
+        }
+     }
+     }
+
+     for(int i=0; i<n; i++){
+
+        cout<<student[i].name<<" "<<student[i].cls<<" "<<student[i].section<<" "<<student[i].id<<" "<<student[i].math_marks<<" "<<student[i].eng_marks<<endl;
+     }
+
+
+    return 0;
+
+}
