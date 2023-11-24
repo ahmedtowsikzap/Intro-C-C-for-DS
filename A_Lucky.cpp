d2e0bad84a9491e84757876427eb4112adb4062d
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-24 18:38:13 +0600

    ICPC Ballons

--- A_Lucky.cpp
+++ A_Lucky.cpp
@@ -1,12 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
-    int t;
+int main() {
+int t;
 
-    cin>>t;
+cin>>t;
+while(t--){
+
+    string s;
+    cin >> s;
+
+    int sum1 = 0;
+    int sum2 = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        
+        if ( i <= 2) {
+            sum1 = sum1 + s[i] - '0';
+        } else {
+            sum2 = sum2 + s[i] - '0';
+        }
+    }
+
+    if(sum1 == sum2){
+        cout<<"YES"<<endl;
+    }
+    else{
+
+        cout<<"NO"<<endl;
+    }
+}
 
     return 0;
-
 }
